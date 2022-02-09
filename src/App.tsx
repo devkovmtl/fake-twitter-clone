d3c64e9f3a31fd18f9e847d92ede6eb65f63b0ca
Mikael <dev.kov.mtl@gmail.com>
2022-02-09 08:30:21 -0500

    Installed react-router-dom and basic config

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { HOME_PATH } from './constants';
 
-function App() {
+const Home = () => {
   return (
     <h1 className='text-blue-500 text-3xl font-bold underline'>
       TWITTER CLONE
     </h1>
   );
-}
+};
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path={HOME_PATH} element={<Home />} />
+    </Routes>
+  );
+};
 
 export default App;
