1ed60c4f9d7ed41523310f7a4bc12c968c53bd69
Mikael <dev.kov.mtl@gmail.com>
2022-02-16 10:53:15 -0500

    app.tsx cleaned

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React, { Children } from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import {
@@ -18,14 +18,8 @@ import {
   ForgoutPassword,
 } from './pages';
 
-const RequireAuth = ({ children }: any) => {
-  let isAuth = null;
-  return isAuth ? children : <Navigate to={`/${SIGNIN_PATH}`} />;
-};
-
 const App = () => {
   const { user } = useUserAuthListener();
-  console.log('APP, USER AUTH', user);
   return (
     <UserContext.Provider value={{ user }}>
       <Routes>
