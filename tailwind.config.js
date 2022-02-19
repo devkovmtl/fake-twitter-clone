62cad9a7c657ae9ddf7cea18e3e4c102066d56af
Mikael <dev.kov.mtl@gmail.com>
2022-02-19 13:37:07 -0500

    updated tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: 'class',
+  content: ['./public/**/*.{html}', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        't-blue': '#1DA1F2',
+        't-blue-dark': '#1A8CD8',
+        't-black': '#14171A',
+        't-dark-gray': '#657786',
+        't-light-gray': '#AAB8C2',
+        't-extra-light-gray': '#E1E8ED',
+        't-extra-extra-light-gray': '#F5F8FA',
+        't-bg-dark': '#243447',
+      },
+    },
   },
   plugins: [require('@tailwindcss/forms')],
 };
