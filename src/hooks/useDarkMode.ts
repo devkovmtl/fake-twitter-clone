5b0b4456a786996759629b1d21a1d374d9f6f817
Mikael <dev.kov.mtl@gmail.com>
2022-02-19 13:42:15 -0500

    Dark Mode Hooks to change the theme

---
+++ src/hooks/useDarkMode.ts
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+
+const useDarkMode = () => {
+  const [isDarkMode, setIsDarkMode] = useState(() => {
+    const isDMode = localStorage.getItem('darkMode') === 'true';
+    return !isDMode || false;
+  });
+
+  useEffect(() => {
+    // grab html to add or remove dark option
+    const root = window.document.documentElement;
+    // check the previous if we are dark then light
+    const previousTheme = isDarkMode ? 'light' : 'dark';
+    root.classList.remove(previousTheme);
+    // next will be opposite of removed
+    const nextTheme = isDarkMode ? 'dark' : 'light';
+    root.classList.add(nextTheme);
+
+    localStorage.setItem('darkMode', `${!isDarkMode}`);
+  }, [isDarkMode]);
+
+  return [isDarkMode, setIsDarkMode];
+};
+
+export default useDarkMode;
